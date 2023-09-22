f0e27746701d3e4f0a6bc23ab52625eb11f35e6f
skartavykh <skartavykh@k-tech.ru>
2023-09-22 18:23:12 +0300

    formation of project structure...

--- utils/page_handler.py
+++ utils/page_handler.py
@@ -1,13 +1,28 @@
 """Module to work with one page. Parse and make some necessary operations to analyse the content of the page."""
 
 import requests
-import urllib
 from bs4 import BeautifulSoup
 import configparser
+from page_analyzer import Analyzer
 
 
 config_parser = configparser.ConfigParser()
 config_parser.read('/home/skartavykh/PycharmProjects/advanced-media/development.ini')
+
+
+class StructuredPage:
+    def __init__(self, content):
+        self.content = content
+
+    def form(self):
+        struct = self.__parse_content()
+        return struct
+
+    def __parse_content(self):
+        return {
+            "page_url": "test_url",
+            "info": "test_info",
+        }
 
 
 class PageHandler:
@@ -15,13 +30,9 @@ class PageHandler:
     def __init__(self, page_url):
         """Initialize page. Get page source HTML code."""
         self.url = page_url
-        try:
-            self.content = requests.get(page_url).content.decode('utf-8')
-        except UnicodeDecodeError:
-            pass
-            # resolved_url = urllib.request.urlopen(page_url).geturl()
-            # self.content = requests.get(resolved_url).content.decode('utf-8')
+        self.content = requests.get(page_url).text
         self.links = []
+        self.structure = StructuredPage(self.content).form()
 
     def generate_file_path(self):
         storage_dir = config_parser['DEFAULT']['STORAGE_PATH']
@@ -41,6 +52,10 @@ class PageHandler:
             link_str: str = link.get('href')
             if link_str and link_str != self.url and link_str.startswith('https://'):
                 self.links.append(link.get('href'))
+
+    def process_page_structure(self):
+        analysis = Analyzer(self.structure)
+        analysis.make_general_analysis()
 
 
 if __name__ == '__main__':
